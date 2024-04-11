b85698ae28e2e033d1baa1fb2a0fb730d8d240c2
spencerxie <spencerxie@tencent.com>
2024-04-11 20:36:54 +0800

    script_1支持不选定任何列的模式

--- script_1.py
+++ script_1.py
@@ -18,27 +18,26 @@ def merge_excel_files(input_list, output_file, output_list):
                         sheet_name = 0
                     # 读取Excel文件的特定工作表
                     data = pd.read_excel(file_name, sheet_name=sheet_name)
-                    # 为输入数据添加一个空列，用于匹配输入为空的case
-                    data[''] = ''
-                    # 按照output_list的顺序重新排列列
-                    data = data[columns]
-                    data_col_len = len(data.columns)
-                    data.columns = output_list[:data_col_len]
-                    # 检查并填充缺失的列
-                    for col in output_list:
-                        if col not in data.columns:
-                            data[col] = None
+
+                    # 选择input_list指定的列
+                    if columns is not None and len(columns) > 0:
+                        # 为输入数据添加一个空列，用于匹配输入为空的case
+                        data[''] = ''
+                        data = data[columns]
+                    if output_list is not None and len(output_list) > 0:
+                        data.columns = output_list[:len(data.columns)]
+                        # 检查并填充缺失的列
+                        for col in output_list:
+                            if col not in data.columns:
+                                data[col] = None
                     # 添加一列来存储文件名
-                    data['SourceFile'] = file_name
+                    data.insert(0, 'SourceFile', file_name)
                     # 将数据添加到merged_data中
                     merged_data = pd.concat([merged_data, data])
                 except Exception as e:
                     print(f"在处理文件 {file_name} 的 {sheet_name} 时发生错误: {e}")
                     traceback.print_exc()  # 打印出错误的详细信息，包括错误发生的行号
                     sys.exit(1)  # 终止程序，返回一个非零的退出状态，表示程序遇到了错误
-
-    # 更改列名
-    merged_data.columns = output_list + ['SourceFile']
 
     # 将合并的数据写入csv/excel文件
     save_data(output_file, merged_data)
@@ -81,11 +80,21 @@ def save_large_data_to_excel(data, output_file):
 global_max_rows = 1048570
 
 if __name__ == '__main__':
-    # 使用示例
+    '''
+    1、sheet名为''时表示使用第一个sheet；
+    2、列字段数组为空数组或者None时表示选择所有的字段；
+    3、列字段为''表示占位空列。
+    '''
     input_list = [{'file1.xlsx': {'': ['a1', 'a2', '', '名字']}},
                   {'file2.xlsx': {'Sheet1': ['b1', 'b2', 'b3', 'b4'], 'Sheet2': ['b6', 'b7', 'b8', 'b9']}},
                   {'file3.xlsx': {'Sheet1': ['c1', 'c2', 'c3', 'c4', 'c5']}}]
+    # output_list 为空数组或者None时表示不修改输出列名
     output_list = ['out1', 'out2', 'out3', 'out4', '哈哈']
+
     # 可以选 scv 也可以选 xlsx 格式
     merge_excel_files(input_list, 'output1.csv', output_list)
-    # merge_excel_files(input_list, 'output1.xlsx', output_list)
+
+    input_list = [{'file4.xlsx': {'': []}},
+                  {'file5.xlsx': {'Sheet1': [], 'Sheet2': []}}]
+    output_list = None
+    merge_excel_files(input_list, 'output1.xlsx', output_list)
